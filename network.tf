5a2e3f91560b3c6d0d37cfbda880ebdaef0fc6d3
Gustavo-RibMartins <guhrm96@gmail.com>
2023-02-15 21:01:39 -0300

    Add: Creating Subnet Config

--- network.tf
+++ network.tf
@@ -1,8 +1,28 @@
 resource "aws_vpc" "vorx-vpc-prod" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "Vorx-PROD"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-pub-1a" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public-Subnet-1a"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1a" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Private-Subnet-1a"
   }
 }
 
@@ -11,5 +31,13 @@ output "vpc_vorx_prod_id" {
 }
 
 output "vpc_vorx_prod_arn" {
-value = aws_vpc.vorx-vpc-prod.arn
+  value = aws_vpc.vorx-vpc-prod.arn
+}
+
+output "vorx_prod_subnet_pub-1a" {
+value = aws_subnet.vorx-subnet-pub-1a.id
+}
+
+output "vorx_prod_subnet_priv-1a" {
+value = aws_subnet.vorx-subnet-priv-1a.id
 }
