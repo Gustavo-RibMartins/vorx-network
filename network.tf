c035ed72d953ca223c40eb05f3091dc9721f2673
Gustavo-RibMartins <guhrm96@gmail.com>
2023-02-15 21:28:30 -0300

    Add: Creating Subnets da AZ 1b

--- network.tf
+++ network.tf
@@ -16,6 +16,16 @@ resource "aws_subnet" "vorx-subnet-pub-1a" {
   }
 }
 
+resource "aws_subnet" "vorx-subnet-pub-1b" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Public-Subnet-1b"
+  }
+}
+
 resource "aws_subnet" "vorx-subnet-priv-1a" {
   vpc_id            = aws_vpc.vorx-vpc-prod.id
   cidr_block        = "10.0.10.0/24"
@@ -23,6 +33,16 @@ resource "aws_subnet" "vorx-subnet-priv-1a" {
 
   tags = {
     Name = "Private-Subnet-1a"
+  }
+}
+
+resource "aws_subnet" "vorx-subnet-priv-1b" {
+  vpc_id            = aws_vpc.vorx-vpc-prod.id
+  cidr_block        = "10.0.20.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Private-Subnet-1b"
   }
 }
 
@@ -40,4 +60,12 @@ value = aws_subnet.vorx-subnet-pub-1a.id
 
 output "vorx_prod_subnet_priv-1a" {
 value = aws_subnet.vorx-subnet-priv-1a.id
+}
+
+output "vorx_prod_subnet_pub-1b" {
+value = aws_subnet.vorx-subnet-pub-1b.id
+}
+
+output "vorx_prod_subnet_priv-1b" {
+value = aws_subnet.vorx-subnet-priv-1b.id
 }
